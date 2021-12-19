8018154ea0c9bad4b7954d0b812a02fba5cf0dbf
EmmaG1 <emmagrace1352@gmail.com>
2021-12-19 15:37:58 +0000

    added array for recipes

--- app/src/main/java/org/wit/recipe/activities/RecipeActivity.kt
+++ app/src/main/java/org/wit/recipe/activities/RecipeActivity.kt
@@ -8,36 +8,63 @@ import org.wit.recipe.models.RecipeModel
 import timber.log.Timber
 import timber.log.Timber.i
 
-
 class RecipeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRecipeBinding
     var recipe = RecipeModel()
+    val recipes = ArrayList<RecipeModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityRecipeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         Timber.plant(Timber.DebugTree())
         i("Recipe Activity started...")
-
-//        binding.btnAdd.setOnClickListener() {
-//            i("add Button Pressed")
-//        }
 
         binding.btnAdd.setOnClickListener() {
             recipe.title = binding.recipeTitle.text.toString()
             if (recipe.title.isNotEmpty()) {
-                i("add Button Pressed: $recipe.title")
+                recipes.add(recipe.copy())
+                i("add Button Pressed: ${recipe}")
+                for (i in recipes.indices)
+                { i("Recipe[$i]:${this.recipes[i]}") }
             }
             else {
-                Snackbar
-                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
     }
 }
+//class RecipeActivity : AppCompatActivity() {
+//
+//    private lateinit var binding: ActivityRecipeBinding
+//    var recipe = RecipeModel()
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityRecipeBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        Timber.plant(Timber.DebugTree())
+//        i("Recipe Activity started...")
+//
+////        binding.btnAdd.setOnClickListener() {
+////            i("add Button Pressed")
+////        }
+//
+//        binding.btnAdd.setOnClickListener() {
+//            recipe.title = binding.recipeTitle.text.toString()
+//            if (recipe.title.isNotEmpty()) {
+//                i("add Button Pressed: $recipe.title")
+//            }
+//            else {
+//                Snackbar
+//                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+//                    .show()
+//            }
+//        }
+//    }
+//}
 
